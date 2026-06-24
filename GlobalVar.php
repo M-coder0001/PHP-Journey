f4129aeb38ef761984aec38054e14715ec88fda7
M-coder0001 <mayankchandrapal@gmail.com>
2026-06-24 08:55:53 +0530

    1 program added

---
+++ GlobalVar.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $a = 10;
+    $b = 20;
+
+    function getvalue() {
+        global $a, $b;
+        $a = $a + 5;
+        $b = $b + 5;
+    }
+    getvalue();
+    echo "a = $a <br> b =  $b";
+    ?>
+</body>
+</html>
