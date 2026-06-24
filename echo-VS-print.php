9fcb83b0eed535a386ecdb3612c25e79fe44a6dd
M-coder0001 <mayankchandrapal@gmail.com>
2026-06-24 08:47:49 +0530

    1 program added

---
+++ echo-VS-print.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php
+    $a = 10;
+    $b = 20;
+
+    echo "The value of a is: " . $a . "<br>";
+    echo "This is PHP" . "This is easy to learn" . "<br>";
+    print "The value of b is: " . $b . "<br>";
+
+    $c = print $b;
+    print "<br> $c";
+    ?>
+
+</body>
+</html>
