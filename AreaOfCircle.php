2c2b6faadf431a5133a31b30175c2dcc3c98464b
M-coder0001 <mayankchandrapal@gmail.com>
2026-06-25 08:17:38 +0530

    New 3 programs added

---
+++ AreaOfCircle.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        define('pi', 3.14);
+        function area($r){
+            $area = pi * $r * $r;
+            echo "Area of Circle = $area";
+        }
+        area(5);
+    ?>
+</body>
+</html>
