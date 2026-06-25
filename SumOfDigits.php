f66d037d4b3aa553cbdc5a9d637691867d4a570d
M-coder0001 <mayankchandrapal@gmail.com>
2026-06-25 08:54:11 +0530

    Addition of first 10 digits

---
+++ SumOfDigits.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php
+        $sum = 0;
+        for($i=1; $i<=10; $i++){
+            $sum = $sum + $i;
+        }
+        echo "Sum of first 10 digits is: $sum <br>";
+    ?>
+
+</body>
+</html>
