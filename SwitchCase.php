fbf2428fe1ab375d15b3af757ff47bf58a13713a
M-coder0001 <mayankchandrapal@gmail.com>
2026-07-01 08:13:11 +0530

    New program added

---
+++ SwitchCase.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $color = 'navy';
+        switch ($color) {
+            case 'yellow':
+                echo "<body bgcolor='yellow'></body>";
+                break;
+            case 'navy':
+                echo "<body bgcolor='navy'></body>";
+                break;
+            case 'red':
+                echo "<body bgcolor='red'></body>";
+                break;
+            default:
+                echo "<body bgcolor='black'></body>";
+                break;
+        }
+    ?>
+</body>
+</html>
