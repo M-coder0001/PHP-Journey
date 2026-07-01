a4a142907ab30278e48e8fb07376b39deba5acbf
M-coder0001 <mayankchandrapal@gmail.com>
2026-07-01 08:02:52 +0530

    New program added

--- EvenOdd.php
+++ EvenOdd.php
@@ -7,7 +7,8 @@
 </head>
 <body>
     <?php
-        
+        $x = 12;
+        echo $x % 2 == 0 ? "<p style='font-size: 35px; color: blue;'>The value of x($x) is even</p>" : "<p style='font-size: 30px; color: yellow;'>The value of x($x) is odd</p>";
     ?>
 </body>
 </html>
