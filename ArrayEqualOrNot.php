f0551b025b74ac8fc1dabeb63d110d32e43620ea
M-coder0001 <mayankchandrapal@gmail.com>
2026-07-08 09:10:33 +0530

    New program added

--- ArrayEqualOrNot.php
+++ ArrayEqualOrNot.php
@@ -2,14 +2,14 @@
     $a = array("a" => 1, "b" => 2, "c" => 3);
     $b = array("c" => 3, "a" => 1, "b" => 2);
 
-    #(==) checks the equality of values in the array, but not the order of the elements.
+    #(==) checks the equality of values and the index in the array, but not the order of the elements.
     if($a == $b){
         echo "Equal";
     }else{
         echo "Not Equal";
     }
     echo "<br>";
-    #(===) checks the equality of values in the array, and also checks the order of the elements.
+    #(===) checks the equality of values and the index in the array, and also checks the order of the elements.
     if($a === $b){
         echo "Equal";
     }else{
