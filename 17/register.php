039e71d094d8164a39886372bd3d42495cd1a560
M-coder0001 <mayankchandrapal@gmail.com>
2026-08-19 08:58:09 +0530

    new programs added

--- 17/register.php
+++ 17/register.php
@@ -32,7 +32,7 @@
             </tr>
             <tr>
                 <td>Mobile Number</td>
-                <td><input type="number" name="mobile" maxlength="10" required></td>
+                <td><input type="text" name="mobile" maxlength="10" required></td>
             </tr>
             <tr>
                 <td>City</td>
@@ -52,5 +52,17 @@
             </tr>
         </table>
     </form>
+    <?php
+        if(isset($_POST['sb'])) {
+            echo "<script>alert('Registration successful!');</script>";
+            
+            echo "<p>Name: " . $_POST['uname'] . "</p>";
+            echo "<p>Email: " . $_POST['email'] . "</p>";
+            echo "<p>Mobile Number: " . $_POST['mobile'] . "</p>";
+            echo "<p>City: " . $_POST['city'] . "</p>";
+
+            echo "Welcome, " . $_POST['uname'] . "! You have successfully registered.";
+        }
+    ?>
 </body>
 </html>
