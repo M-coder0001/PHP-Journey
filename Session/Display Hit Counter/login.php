d48804c091e3d89632a3bfdbb08be705e6740c49
M-coder0001 <mayankchandrapal@gmail.com>
2026-09-09 08:22:33 +0530

    new programs added

---
+++ Session/Display Hit Counter/login.php
@@ -0,0 +1,25 @@
+<?php
+    header("Cache-Control: no-cache, no-store, must-revalidate");
+?>
+
+<html>
+    <head>
+        <title>Login</title>
+    </head>
+    <body>
+        <form> 
+            Username: <input type="text" name="username"><br>
+            Password: <input type="password" name="password"><br>
+            <input type="submit" name="sb" value="Login">
+        </form>
+        <?php
+        
+            if (isset($_POST['sb'])) 
+            {
+                session_start();
+                $_SESSION['username'] = $_POST['username'];
+                header("Location: home.php");   
+            }
+        ?>
+    </body>
+</html>
