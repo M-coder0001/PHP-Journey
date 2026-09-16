82bba240d661ecb7742509d97099a7260c58e3fc
M-coder0001 <mayankchandrapal@gmail.com>
2026-09-16 08:19:12 +0530

    new programs added

--- MethodOverriding.php
+++ MethodOverriding.php
@@ -1,3 +1,32 @@
 <?php
-    
+    class car
+    {
+        function display()
+        {
+            echo "This is car class";
+        }
+    }
+    class sportsCar extends car
+    {
+        function display()
+        {
+            echo "<br>This is sportsCar class";
+        }
+    }
+    class LuxuryCar extends sportsCar
+    {
+        function display()
+        {
+            echo "<br>This is LuxuryCar class";
+        }
+    }
+
+    $a = new car();
+    $a->display();
+
+    $b = new sportsCar();
+    $b->display();
+
+    $c = new LuxuryCar();
+    $c->display();
 ?>
